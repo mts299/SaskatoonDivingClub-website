c7f7f8394edc1ded0c7e2bb3af794c49168c726d
mts299 <marina.t.schmidt@gmail.com>
2020-10-25 20:24:05 -0600

    updating front page to a slide show

--- js/album.js
+++ js/album.js
@@ -15,15 +15,34 @@ $(document).ready(function () {
   new WOW().init();
 });
 
-var images=new Array('./imgs/Jamie.png','./imgs/Margo_quin_syncro.png');
-var nextimage=0;
-doSlideshow();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function doSlideshow(){
-    if(nextimage>=images.length){nextimage=0;}
-    $('.dive')
-    .css('background-image','url("'+images[nextimage++]+'")')
-    .fadeIn(60,function(){
-        setTimeout(doSlideshow,10000);
-    })
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
